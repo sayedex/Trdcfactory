f1ecd9f5e761ebd55409eb88b7281abf7586c45c
sayedex <sayedadmod@gmail.com>
2022-08-11 13:56:27 +0600

    first commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,44 +1,60 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { TrdcFactory, newpool } from "../generated/TrdcFactory/TrdcFactory"
-import { ExampleEntity } from "../generated/schema"
+import { Address, BigInt ,dataSource} from "@graphprotocol/graph-ts"
 
+import { TrdcFactory, newpool } from "../generated/TrdcFactory/TrdcFactory"
+import {
+  Deposit
+} from "../generated/templates/SmartChefInitializable/SmartChefInitializable"
+import { facto,Pool ,User} from "../generated/schema"
+let ZERO_BI = BigInt.fromI32(0);
+let ONE_BI = BigInt.fromI32(1);
 export function handlenewpool(event: newpool): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let factory = facto.load(event.transaction.hash.toHex());
+  let pool = Pool.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  if (!pool) {
+    pool = new Pool(event.params.Pooladresss.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.Pooladresss = event.params.Pooladresss
-
+  if(!factory){
+     factory = new facto(event.transaction.hash.toHex());
+  
+  }
+  factory.Factoryaddress = event.params.Pooladresss;
+  factory.totalSmartChef = factory.totalSmartChef.plus(ONE_BI)
+  // pool.amount = BigInt.fromI32(0)
+ pool.Staketoken = event.address;
+ pool.rewardtoken = event.address;
+ pool.factoryad = event.params.Pooladresss;
+  //pool.create(event.params)
   // Entities can be written to the store with `.save()`
-  entity.save()
+  factory.save()
+  pool.save()
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.poolad(...)
+
+
+}
+
+export function handleDeposit(event: Deposit): void {
+let id  = event.transaction.hash.toHex();
+  let pool = Pool.load(event.transaction.hash.toHex());
+  if (!pool) {
+    pool = new Pool(event.transaction.hash.toHex())
+
+  }  
+  let user = User.load(event.transaction.hash.toHex());
+   
+  if (!user) {
+    user = new User(event.transaction.hash.toHex())
+
+  }
+user.stakeAmount = event.params.amount;
+user.address= event.transaction.from;
+user.pool  = pool.id;
+pool.save();
+user.save();  
 }
